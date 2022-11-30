924aa96d8f18ad294fab2cc7d8b988969b04828a
naethanyoull <116758262+naethanyoull@users.noreply.github.com>
2022-11-30 15:13:58 -0500

    Create heavyIsDead.java

---
+++ heavyIsDead.java
@@ -0,0 +1,202 @@
+/*Heavy: Ya-da-da-da-da-da- It is good day to be not dead!
+
+Engineer: POW! You are dead!
+
+Heavy: I am dead!
+
+Engineer: Chuckling, while spy is doing the conga towards the scene (The Engineer says aw, shucks” as the Spy gets close.)
+
+Spy: Oh! The Heavy is dead!
+
+Heavy: Yes (Sandwich). I am dead!
+
+
+Spy: Why is the Heavy dead?!
+
+Engineer: I dunno.
+
+Heavy: I think it was-
+
+Engineer & Spy: Shhh, you are dead!
+
+Heavy: Ok.
+
+(Sniper enters scene exiting van)
+
+Sniper: What's up, you wankers?! Who’s up for a- AH! What the- bloody hell just happened?!
+
+Engineer & Spy: The Heavy is dead!
+
+(A, B, C, or D game starts)
+
+Sniper: The Heavy is dead!
+
+Spy: Correct!
+
+(Option C lights up and celebration music starts playing)
+
+Spy: So, did you see the murderer?
+
+
+Engineer and Sniper: Nah, sorry mate.
+
+Spy: Slams hand on desk I will find him, I will capture him, and no one will ever die again!
+
+(Engineer and Sniper applause)
+
+Sniper: Ah, well that's nice.
+
+Engineer: I am damn proud right now.
+
+(Soldier appears in scene)
+
+Soldier: Atteeeeeeeeention!
+
+(Soldier rushes to dead Heavy)
+
+Soldier: That Heavy is dead!
+
+Spy: We know!
+
+Soldier: Who killed him?!
+
+Spy: We don’t know!
+
+Soldier: I will find clues!
+
+
+(Soldier searches through pile of stuff while sniffing)
+
+Soldier: What's that? Grabs gun A weapon?! That thing is why the Heavy is dead!
+
+Engineer, Sniper, and Spy: The Heavy is dead?!
+
+Soldier: Slams hands on desk Yes, (Intense background appears) he died!
+
+Engineer, Sniper, and Spy: All shocked
+
+(Intense background stops, showing the Engineer, Sniper, and Spy standing in front of a green screen)
+
+Medic: (From far away) Incoming!
+
+(Ambulance crashes Soldier into a building wall, killing him.)
+
+Medic: Exits out of Ambulance Raus, raus!
+Medic pushes Engineer, Sniper, and Spy away from Heavy
+
+Medic: Move now!
+Medic kisses Heavy on the head. Heavenly music sounds while the light focuses on Heavy as he rises in the air.
+
+Heavy: Hohoho, Hea- Explodes Oof.
+
+Medic: In my medical opinion, that Heavy is dead!
+
+(Camera rotates to Sniper)
+
+Sniper: Doc, what happened?
+
+Medic: My professional opinion? Slams hands on desk then turns to the right (Intense background comes in) The Heavy was killed!
+
+(Intense background stops)
+
+Engineer, Sniper, and Spy: Panicking
+
+Medic: I don’t think it’s anything to worry about.
+
+Spy: Well, now what?
+
+(Scout entering while doing the conga in the background)
+
+Scout: Clipidy clop mother****er! Boom!
+
+Spy: Says “Oh, come on.” before Scout says boom
+
+Scout: Look at this! The freaking Heavy is dead!
+
+Pause
+
+Scout: What do you think of that?
+
+Pause
+
+Scout: Ahm…
+
+Spy: Yes, yes, Scout.
+
+Scout: Yea?
+
+Spy: Go home!
+
+Scout’s mother appears in a car telling Scout to get in
+
+Scout: Ah come on! Pffff! Freaking unbelievable seriously, you all suck.
+
+Car drives off and crashing noise is heard
+
+Scout: Screams of pain
+
+Spy: Ok, let’s get back to the point.
+
+Heavy: Poking at his dead body I think Heavy is dead.
+
+Engineer, Sniper, Spy, and Medic: The Heavy is dead?!
+
+Medic: Turns around and notices dead scout in burning car Scout! I can heal yo-
+
+Car explodes and camera goes back to Heavy
+
+Heavy: Oh, Seriously?! Who killed Heavy?!
+
+Camera shows Demoman drinking Scrumpy
+
+Demoman: Slurp Slurp It was me!
+
+Engineer, Sniper, Spy, and Heavy: Shocked
+
+Demoman: Yes! Bottle of Scrumpy slides up Demoman’s body and Demoman eats the bottle I did it like this: Takes out a revolver and shoots Sniper in chest Boom! Demoman flies off from force of revolver blast
+
+Sniper: Screams of pain
+
+Demoman: Woop dee doo!
+
+Engineer, Spy, and Heavy: Are terrified seeing Sniper’s dead body
+
+Demoman: Burps That’s a joke, lads.
+
+Engineer, Spy, and Heavy: Starts laughing like crazy
+
+Demoman: Slurp Slurp Burp It was… yo-... Burp Points at Engineer Him!
+
+Engineer: Shocked How did you know?!
+
+Demoman: I didn’t. Burps That was a joke too.
+
+Camera moves to Engineer, as the Demoman keeps drinking faster
+
+Demoman: Falls on ground Oh, I’m dead.
+
+Engineer: Manic laughter That’s right! It was me!
+
+Spy: You monster!
+
+Heavy: But whyyyyy?
+
+Engineer: Cause you’re fat, boy. And another thing, you’re ugly.
+
+Heavy: Engineer, stop!
+
+Engineer and Heavy: Arguing
+
+Spy: Shrugs (Text appears saying : IT’S TRADITION)
+
+Engineer: Ah dammit Heavy **** off! You are dead.
+
+Heavy: No u, POW! Haha.
+
+Engineer falls dead on floor
+
+Heavy: You are dead! Not big surprise.
+
+Spy: Well that was idiotic. Off to hang myself! Spy doing flip on noose Watch and lea- Choking noises
+
+Heavy: I am alive! Is nice. Yes, this is stupid. */
